f23d412ea0b9ab296830603c443a4d662d412cea
Emmylong1 <emmanuelibok505@gmail.com>
2025-12-26 22:50:15 +0100

    prevent invalid index errors caused by mismatched counts

--- laravel-10-boilerplate-infra-task/modules/vpc/main.tf
+++ laravel-10-boilerplate-infra-task/modules/vpc/main.tf
@@ -72,6 +72,22 @@ resource "aws_nat_gateway" "nat" {
 
 
 # Route Tables
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+
 resource "aws_route_table" "private" {
   count  = 1
   vpc_id = aws_vpc.main.id
