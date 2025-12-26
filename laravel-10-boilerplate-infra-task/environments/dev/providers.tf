6c7618a6586d71a6a572f73423b1c6f843871d44
Emmylong1 <emmanuelibok505@gmail.com>
2025-12-26 19:51:06 +0100

    fix requested bucket from us-east-1 actual location us-west-2

--- laravel-10-boilerplate-infra-task/environments/dev/providers.tf
+++ laravel-10-boilerplate-infra-task/environments/dev/providers.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "laravel-10-boilerplate-bucket"
     key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
   }
 }
 
