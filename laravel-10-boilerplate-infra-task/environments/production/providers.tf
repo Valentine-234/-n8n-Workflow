4c47b671d84895bb98579a7d126f1d6e8b50d973
Emmylong1 <emmanuelibok505@gmail.com>
2025-12-26 18:13:16 +0100

    use GitLab OIDC to assume AWS IAM role via STS in every job

--- laravel-10-boilerplate-infra-task/environments/production/providers.tf
+++ laravel-10-boilerplate-infra-task/environments/production/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "laravel-10-boilerplate-bucket"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
